decbb1624b4c20c45c8b48a3606b768139469ff1
Paz Gerti <pazgerti4@gmail.com>
2019-11-07 09:51:30 +0200

    app bussss

--- js/apps/mister-email/cmps/email-short-details.cmp.js
+++ js/apps/mister-email/cmps/email-short-details.cmp.js
@@ -1,16 +1,27 @@
 'use strict'
 
+import emailService from '../services/email-service.js'
+import {eventBus} from '../../../general-service/event-bus-service.js'
+
 export default {
     props:['email'],
     template:`
     <section class="emails-short-details-container">
-        
-
-        <div><button>Delete</button><router-link :to="'email/'+email.id"><button>Read More</button></router-link></div>
+        <div><button @click="deleteEmail(email.id)">Delete</button><router-link :to="'email/'+email.id"><button>Read More</button></router-link><button @click="emailToKeep(email)">Keep</button></div>
         <h2>{{email.subject}}</h2>
         <h4>Name</h4><span>User Mail</span>
         <p>{{email.body}}</p>
-    </section>
-    `
+    </section>`
+    ,
+    methods:{
+        deleteEmail(emailId){
+            emailService.deleteMail(emailId);
+        },
+        emailToKeep(email){
+            eventBus.$emit('email-toKeep',email)
+
+        }
+
+    }
 
 }
--- js/apps/mister-email/pages/email-app.cmp.js
+++ js/apps/mister-email/pages/email-app.cmp.js
@@ -2,6 +2,7 @@
 
 import emailHeader from '../cmps/email-app-header.cmp.js'
 import emailFilter from '../cmps/email-filter.cmp.js'
+import userMsg from '../cmps/user-msg.cmp.js'
 
 
 export default {
@@ -9,14 +10,15 @@ export default {
     <section>
         <h1>Emails!</h1>
         <email-filter></email-filter>
-
         <email-header></email-header>
-        
+        <user-msg></user-msg>
+
         <router-view></router-view>
     </section>`
     ,
     components: {
         emailFilter,
-        emailHeader
+        emailHeader,
+        userMsg
     }
 }
