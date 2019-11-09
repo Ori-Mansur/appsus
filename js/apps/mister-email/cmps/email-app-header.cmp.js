8df03d2e24c502321ba6296c33c329b28cd1718b
Paz Gerti <pazgerti4@gmail.com>
2019-11-09 17:50:19 +0200

    add css 050

--- js/apps/mister-email/cmps/email-app-header.cmp.js
+++ js/apps/mister-email/cmps/email-app-header.cmp.js
@@ -5,24 +5,50 @@ import {eventBus} from '../../../general-service/event-bus-service.js'
 
 export default {
     template:`
-    <section class="email-app-header-container">
+    <section v-if="checkWidth" class="email-app-header-container">
         <div class="compose"><router-link to="/email/compose">Compose</router-link></div>
         <nav>
             <router-link class="inbox-link" to="/email">Inbox <span v-if="unreadMails" class="unread-mails-counter"> {{unreadMails}}</span></router-link>
-            <router-link class="draft-link" to="/email/draft">Drafts</router-link>
             <router-link class="starred-link" to="/email/starred">Starred</router-link>
+            <router-link class="draft-link" to="/email/draft">Drafts</router-link>
         </nav>
         <email-status></email-status>
-    </section>`,
+    </section>
+    <section v-else @click="toggleLinks" class="email-app-header-container-small"><span class="menu-btn">☰</span>
+        <div class="cover-links" v-if="openModal">
+        <nav class="links-small-header">
+            <router-link class="inbox-link" to="/email">Inbox <span v-if="unreadMails" class="unread-mails-counter"> {{unreadMails}}</span></router-link>
+            <router-link class="draft-link" to="/email/draft">Drafts</router-link>
+            <router-link class="starred-link" to="/email/starred">Starred</router-link>
+            <router-link to="/email/compose" >Compose</router-link>
+        </nav>
+        </div>
+
+    </section>
+    `,
     data(){
         return {
-            unreadMails: 0
+            unreadMails: 0,
+            openModal:false,
+            width: 0 
         }
     },
     created(){
+        this.width=document.querySelector('body').clientWidth
         eventBus.$on('update-unread',(counter)=>{
             this.unreadMails = counter
         })
+    },
+    methods:{
+        toggleLinks(){
+            this.openModal = !this.openModal
+        }
+    },
+    computed:{
+        checkWidth(){
+            if(this.width >630) return true;
+            return false
+        }
     },
     components: {
         emailStatus
