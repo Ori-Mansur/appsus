9d135759a2890892753b3ca4bd34d1a3c667b5a7
Paz Gerti <pazgerti4@gmail.com>
2019-11-10 11:24:50 +0200

    text

--- js/general-cmps/header.cmp.js
+++ js/general-cmps/header.cmp.js
@@ -6,30 +6,48 @@ export default {
     <header>
     <div class="main-header">
     <h2 class="logo">APPSUS</h2>
-    <button @click="toggleMenu" class="main-nav-btn">☰</button>
-    
-    <nav v-show="isMenu">
+
+    <nav v-if="checkWidth">
         <router-link to="/" class="main-nav-a" exact @click.native="toggleMenu">Home</router-link> 
         <router-link to="/email" class="main-nav-a" @click.native="toggleMenu">Email</router-link> 
         <router-link to="/notes" class="main-nav-a" @click.native="toggleMenu">Notes</router-link> 
         <router-link to="/book" class="main-nav-a" @click.native="toggleMenu">Books</router-link> 
         <router-link to="/about" class="main-nav-a" @click.native="toggleMenu">About</router-link> 
     </nav>
+    <div v-else>
+        <button @click="toggleMenu" class="main-nav-btn">☰</button>
+    
+        <nav v-show="isMenu">
+            <router-link to="/notes" class="main-nav-a" @click.native="toggleMenu">Notes</router-link> 
+            <router-link to="/" class="main-nav-a" exact @click.native="toggleMenu">Home</router-link> 
+            <router-link to="/email" class="main-nav-a" @click.native="toggleMenu">Email</router-link> 
+            <router-link to="/book" class="main-nav-a" @click.native="toggleMenu">Books</router-link> 
+            <router-link to="/about" class="main-nav-a" @click.native="toggleMenu">About</router-link> 
+        </nav>
+    </div>
     </div>
     </header>
     `,
     data() {
         return {
             isMenu: false,
+            width: 0
+        }
+    },
+    computed:{
+        checkWidth(){
+            if(this.width>630) return true;
+            return false;
         }
     },
     methods: {
         toggleMenu() {
-            if (screen.width > 630) return
             this.isMenu = !this.isMenu
+            
         }
     },
     created() {
-        if (screen.width > 630) this.isMenu = true
+        this.width = document.querySelector('body').clientWidth;
+        if(this.width<630)  this.width = false
     },
 }
