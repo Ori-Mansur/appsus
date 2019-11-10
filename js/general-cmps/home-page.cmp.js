5c2a9afea492bb9a1dc3da03d83cea7f4826b67e
Ori Mansur <orimansur@gmail.com>
2019-11-10 18:49:58 +0200

    fix

--- js/general-cmps/home-page.cmp.js
+++ js/general-cmps/home-page.cmp.js
@@ -8,15 +8,15 @@ export default {
         <div class="main-card-container">
             <router-link to="/email" class="main-card-a"> 
                 <img src="img/pexels-photo-3153198.jpeg">
-                Contact your team with our <h3>Email</h3>
+              <div>  Contact your team with our <h3>Email</h3> Save email as note</div>
             </router-link> 
             <router-link to="/notes" class="main-card-a">
                 <img src="img/pexels-photo-669615.jpeg">
-                Keeping your stuff in order with our <h3>Notes</h3> worth pin the important ones up
+               <div> Keeping your stuff in order with our <h3>Notes</h3> worth pin the important ones up</div>
             </router-link> 
             <router-link to="/book" class="main-card-a">
                 <img src="img/pexels-photo-357547.jpeg">
-                Have more time for yourself find your next book in our <h3>Books</h3> provider
+               <div> Have more time for yourself find your next book in our <h3>Books</h3> provider</div>
             </router-link> 
         </div>
     </section>
